9453c674a05af23b41bfa8ff9c26c0bb548840f7
JP Dillingham <jpdillingham@gmail.com>
2022-05-07 11:59:09 -0500

    list files

--- cypress/integration/1-getting-started/todo.spec.js
+++ cypress/integration/1-getting-started/todo.spec.js
@@ -32,6 +32,7 @@ describe('example to-do app', { canPropsAlsoGoInSuite: true }, () => {
     // and then perform an assertion with `should`.
     cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
     cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+    expect(true).to.equal(false);
   })
 
   // it('can add new todo items', () => {
